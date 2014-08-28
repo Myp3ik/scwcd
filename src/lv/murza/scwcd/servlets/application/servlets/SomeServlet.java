91d05b47d2ac8901608e77e8fb16238ff3526b3f
Myp3ik <mp3ik@inbox.lv>
2014-08-28 18:30:05 +0300

    all files for now

---
+++ src/lv/murza/scwcd/servlets/application/servlets/SomeServlet.java
@@ -0,0 +1,23 @@
+package lv.murza.scwcd.servlets.application.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter pw = resp.getWriter();
+        pw.write("<html><head></head><body><form method='post'><input type='submit' value='gfdgdfgdfg'/></form></body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter pw = resp.getWriter();
+        pw.write("<html><head></head><body><form method='post'><input type='submit' value='gfdgdfgdfg'/></form></body></html>");
+    }
+}
