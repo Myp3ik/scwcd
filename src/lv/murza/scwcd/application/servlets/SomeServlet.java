abc78deaa9986ed60654fffe249b58648074f9f7
Myp3ik <mp3ik@inbox.lv>
2014-09-01 17:42:00 +0300

    httponly cookie & secure cookie

--- src/lv/murza/scwcd/application/servlets/SomeServlet.java
+++ src/lv/murza/scwcd/application/servlets/SomeServlet.java
@@ -1,6 +1,7 @@
 package lv.murza.scwcd.application.servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,6 +19,19 @@ public class SomeServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter pw = resp.getWriter();
-        pw.write("<html><head></head><body><form method='post'><input type='submit' value='gfdgdfgdfg'/></form></body></html>");
+        pw.write("<html><head></head><body><form method='post'><input type='submit' value='654'/></form></body></html>");
+        
+        Cookie cookie = new Cookie("POST_1", "simpleCookie");
+        resp.addCookie(cookie);   
+        
+        cookie = new Cookie("POST_2", "httpOblyCookie");
+        cookie.setHttpOnly(true);
+        resp.addCookie(cookie);
+        
+        cookie = new Cookie("POST_3", "secureCookie");
+        cookie.setSecure(true);
+        resp.addCookie(cookie);
+                
+        req.getSession(true);
     }
 }
